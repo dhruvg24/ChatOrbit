42fd3d0a5422c51251c03abc5683b1577f79b19f
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2024-02-23 12:24:39 +0530

    Home page partial completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 // import Login from "./components/login/Login.jsx";
-import SignUp from "./components/signup/SignUp.jsx";
+// import SignUp from "./components/signup/SignUp.jsx";
+import Home from "./pages/home/Home.jsx";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center ">
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Home />
     </div>
   );
 }
---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SearchInput from './SearchInput.jsx'
+import Conversations from './Conversations.jsx'
+import LogoutButton from './LogoutButton.jsx'
+
+const Sidebar = () => {
+  return (
+    // will have a search bar on top
+    // then the online users 
+    // and at bottom the logout button
+
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+        <SearchInput/>
+        
+        <div className='divider px-3'>
+        </div>
+
+        <Conversations />
+        <LogoutButton/>
+    </div>
+    
+  )
+}
+
+export default Sidebar
