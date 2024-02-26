83cc709991564209143c33553a93e35f1a20a3a1
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2024-02-27 00:32:41 +0530

    get conversation done

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import useConversation from "../../zustand/useConversation.js";
 
 const Conversation = ({conversation, lastIdx, emoji}) => {
+
+  const {selectedConversation, setSelectedConversation} = useConversation()
+
+  const isSelected = selectedConversation?._id === conversation._id
+
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+      <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected?"bg-sky-400":""}`} onClick={()=>setSelectedConversation(conversation)}>
         <div className="avatar online">
           <div className="w-12 rounded-full">
             <img
