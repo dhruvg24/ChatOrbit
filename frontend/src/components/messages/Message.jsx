3175c319c5b6ec3dd1154afab8ace60157dbf1f9
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2024-03-02 14:58:38 +0530

    message rendering and skeleton added

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import { useAuthContext } from "../../context/AuthContext.jsx";
+import { extractTime } from "../../utils/extractTime.js";
+import useConversation from "../../zustand/useConversation.js";
+const Message = ({ message }) => {
+  // check for message from us or other user?
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
 
-const Message = () => {
+  // in messages db - we have sender and receiver id
+  const msgFromMe = message.senderId === authUser._id;
+
+  const formattedTime = extractTime(message.createdAt) 
+  // for different appearance of user sender-receiver
+  const chatClassName = msgFromMe ? "chat-end" : "chat-start";
+
+  // for profile pic of sender-receiver
+  const profilePic = msgFromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+
+  const bubbleBgColor = msgFromMe ? "bg-blue-500" : "";
+
   return (
-<div className='chat chat-end'>
-			<div className='chat-image avatar'>
-				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src= {"https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"} />
-				</div>
-			</div>
-			<div className={`chat-bubble text-white bg-blue-500`}>Hii! whats up?</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:15</div>
-		</div>
-  )
-}
+    <div className={`chat ${chatClassName}`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        {formattedTime}
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message.jsx";
+import useGetMessages from "../../hooks/useGetMessages.js";
+import MessageSkeleton from "../skeletons/MessageSkeleton.jsx";
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  // for automatic scroll if screen covers with msg - to display recent msgs
+  const lastMsgRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      lastMsgRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
+  // whenever messages array updates it targets the ref as lastMsgRef and points it out after scroll.
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {/* rendering the messages */}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMsgRef}>
+            <Message message={message} />
+          </div>
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start conversation</p>
+      )}
     </div>
   );
 };
