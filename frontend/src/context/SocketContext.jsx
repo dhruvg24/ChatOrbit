0aed691e18f65c3343dc315f99e3bead85eef4bb
dhruvg24 <dhruvgarg24112002@gmail.com>
2024-03-03 20:55:45 +0530

    socketcontext io path update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatorbit-deploy.onrender.com", {
+      const socket = io("https://chatorbit-production.onrender.com", {
         query : {
             userId: authUser._id
         }
